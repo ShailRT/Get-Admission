956168afc8fbffaacaee5d922e720196af0a67f1
ShailRT <shailesh18h@gmail.com>
2023-05-21 18:41:52 +0530

    21may

--- cms/admin.py
+++ cms/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Gallery, Author, College
 
+class CollegeAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'location']
+    list_filter = ['institute_type']
+
+
 admin.site.register(Gallery)
 admin.site.register(Author)
-admin.site.register(College)
+admin.site.register(College, CollegeAdmin)
